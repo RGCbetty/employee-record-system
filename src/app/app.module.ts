5930e8cab14603a1f7e9708adf78dcc887224294
adriancalma <adrian.calma@yahoo.com>
2021-11-25 15:54:57 +0800

    add employee modal folder

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { EmployeeModalComponent } from './employee/employee-modal/employee-modal.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EmployeeModalComponent
   ],
   imports: [
     BrowserModule
